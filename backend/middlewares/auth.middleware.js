b8c8f1fef4c25bcebc888d7bd8c60842c286727d
Mike Godfrinne <mike.godfrinne.dev@gmail.com>
2024-04-24 15:48:25 +0200

    Add JWT authentication and authorization
    A authentication middleware was created to decode JWT tokens, and an authorization middleware was created to verify user roles. New variables related to JWT were also added to the *.env.example* file which are used within the JWT utility functions for generating and decoding tokens.

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,47 @@
+import { decodeJwt } from '../utils/jwt.utils.js';
+
+export const authTokenMiddleware = () => {
+
+    return (req, res, next) => {
+
+        const authHeader = req.headers['authorization'];
+
+        const token = authHeader && authHeader.split(' ')[1];
+
+        if(!token) {
+            next();
+            return;
+        }
+
+        decodeJwt(token)
+            .then((data) => {
+                req.token = data;
+            })
+            .catch((error) => {
+                req.token = null;
+            })
+            .finally(() => {
+                next();
+            });
+    }
+}
+
+export const authorizeMiddleware = (...roles) => {
+
+    return (req, res, next) => {
+
+        const token = req.token;
+
+        if(!token) {
+            res.sendStatus(401);
+            return;
+        }
+
+        if(roles.length > 0 && !roles.includes(token.role)) {
+            res.sendStatus(403);
+            return;
+        }
+
+        next();
+    }
+}
